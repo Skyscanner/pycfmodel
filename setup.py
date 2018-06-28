3abf553af071042ab46038474b4e2c57cba80f90
Valentin <v.tunev@outlook.com>
2018-06-28 13:27:28 +0300

    Add Travis, bump pip-tools version

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 install_requires = []
 
@@ -7,7 +9,7 @@ dev_requires = [
     'pytest==3.5.0',
     'flake8>=3.3.0',
     'pytest-cov>=2.5.1',
-    'pip-tools==1.10.1',
+    'pip-tools==2.0.2',
 ]
 
 setup(
@@ -16,7 +18,9 @@ setup(
     description='A python model for CloudFormation scripts',
     author='Skyscanner Product Security',
     author_email='security@skyscanner.net',
-    url='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Skyscanner/pycfmodel',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=install_requires,
     tests_require=dev_requires,
