0d9402105e9e44b110a493756a1ed6a894ede850
Valentin <hdbuster@gmail.com>
2018-07-24 16:20:34 +0300

    Skip dict actions for now

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dev_requires = [
 
 setup(
     name='pycfmodel',
-    version='0.2.4',
+    version='0.2.5',
     description='A python model for CloudFormation scripts',
     author='Skyscanner Product Security',
     author_email='security@skyscanner.net',
