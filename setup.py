b08d27b1a913c0c5eea04571dbfc7aceaebd8232
Valentin <hdbuster@gmail.com>
2019-02-15 15:57:06 +0200

    Fix resource key

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dev_requires = [
 
 setup(
     name='pycfmodel',
-    version='0.2.9',
+    version='0.2.10',
     description='A python model for CloudFormation scripts',
     author='Skyscanner Product Security',
     author_email='security@skyscanner.net',
