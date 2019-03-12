fcd0f3cc45b2b72e33b61fa9c47c2731788c7983
David Bryant <david.bryant@cfacorp.com>
2019-03-12 06:41:41 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dev_requires = [
 
 setup(
     name='pycfmodel',
-    version='0.2.11',
+    version='0.2.12',
     description='A python model for CloudFormation scripts',
     author='Skyscanner Product Security',
     author_email='security@skyscanner.net',
