1b129171fd0eddb37b53197ed74b4eec32ac5508
Oscar Blanco <oscarbc1996@gmail.com>
2019-09-19 12:48:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dev_requires = [
 
 setup(
     name='pycfmodel',
-    version='0.3.2',
+    version='0.3.3',
     description='A python model for CloudFormation scripts',
     author='Skyscanner Product Security',
     author_email='security@skyscanner.net',
