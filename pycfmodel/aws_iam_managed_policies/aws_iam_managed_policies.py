03b53fe00ac9b827178dcf824bf049f45ee513d0
Oscar Blanco <oscarbc1996@gmail.com>
2020-03-09 09:38:06 +0100

    add description

--- pycfmodel/aws_iam_managed_policies/aws_iam_managed_policies.py
+++ pycfmodel/aws_iam_managed_policies/aws_iam_managed_policies.py
@@ -12,6 +12,9 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
+# Script to get a local copy of AWS IAM Managed Policies.
+
 import boto3
 
 from pathlib import Path
