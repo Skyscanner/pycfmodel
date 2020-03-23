87644fed9321b0fd8d1a5bbb23b81842c15b08c0
Oliver Crawford <oliver.crawford@skyscanner.net>
2020-03-23 16:50:00 +0000

    reset tests back to before PR times, except where Pydantic now fails it

--- tests/resources/test_security_group_egress.py
+++ tests/resources/test_security_group_egress.py
@@ -22,7 +22,13 @@ def security_group_egress_ipv4_1():
     return SecurityGroupEgress(
         **{
             "Type": "AWS::EC2::SecurityGroupEgress",
-            "Properties": {"GroupId": "sg-1", "CidrIp": "0.0.0.0/0", "FromPort": 41, "ToPort": 45, "IpProtocol": "tcp"},
+            "Properties": {
+                "GroupId": "sg-12341234",
+                "CidrIp": "0.0.0.0/0",
+                "FromPort": 41,
+                "ToPort": 45,
+                "IpProtocol": "tcp",
+            },
         }
     )
 
@@ -32,18 +38,24 @@ def security_group_egress_ipv4_2():
     return SecurityGroupEgress(
         **{
             "Type": "AWS::EC2::SecurityGroupEgress",
-            "Properties": {"GroupId": "sg-2", "CidrIp": "172.0.0.0", "FromPort": 41, "ToPort": 45, "IpProtocol": "tcp"},
+            "Properties": {
+                "GroupId": "sg-2345",
+                "CidrIp": "172.0.0.0",
+                "FromPort": 41,
+                "ToPort": 45,
+                "IpProtocol": "tcp",
+            },
         }
     )
 
 
 @pytest.fixture()
-def security_group_egress_ipv6_1():
+def security_group_egress_ipv6():
     return SecurityGroupEgress(
         **{
             "Type": "AWS::EC2::SecurityGroupEgress",
             "Properties": {
-                "GroupId": "sg-1",
+                "GroupId": "sg-12341234",
                 "CidrIpv6": "fc00::/7",
                 "FromPort": 41,
                 "ToPort": 45,
@@ -54,7 +66,7 @@ def security_group_egress_ipv6_1():
 
 
 def test_security_group_egress(security_group_egress_ipv4_1):
-    assert security_group_egress_ipv4_1.Properties.GroupId == "sg-1"
+    assert security_group_egress_ipv4_1.Properties.GroupId == "sg-12341234"
     assert isinstance(security_group_egress_ipv4_1.Properties.FromPort, int)
 
 
@@ -66,5 +78,5 @@ def test_not_slash_zero4(security_group_egress_ipv4_2):
     assert security_group_egress_ipv4_2.ipv4_slash_zero() == False
 
 
-def test_not_slash_zero6(security_group_egress_ipv6_1):
-    assert security_group_egress_ipv6_1.ipv6_slash_zero() == False
+def test_not_slash_zero6(security_group_egress_ipv6):
+    assert security_group_egress_ipv6.ipv6_slash_zero() == False
--- tests/resources/test_security_group_ingress.py
+++ tests/resources/test_security_group_ingress.py
@@ -22,7 +22,13 @@ def security_group_ingress_ipv4_1():
     return SecurityGroupIngress(
         **{
             "Type": "AWS::EC2::SecurityGroupIngress",
-            "Properties": {"GroupId": "sg-1", "CidrIp": "0.0.0.0/0", "FromPort": 46, "ToPort": 46, "IpProtocol": "tcp"},
+            "Properties": {
+                "GroupId": "sg-12341234",
+                "CidrIp": "0.0.0.0/0",
+                "FromPort": 46,
+                "ToPort": 46,
+                "IpProtocol": "tcp",
+            },
         }
     )
 
@@ -32,19 +38,25 @@ def security_group_ingress_ipv4_2():
     return SecurityGroupIngress(
         **{
             "Type": "AWS::EC2::SecurityGroupIngress",
-            "Properties": {"GroupId": "sg-2", "CidrIp": "127.0.0.0", "FromPort": 46, "ToPort": 46, "IpProtocol": "tcp"},
+            "Properties": {
+                "GroupId": "sg-2345",
+                "CidrIp": "127.0.0.0",
+                "FromPort": 46,
+                "ToPort": 46,
+                "IpProtocol": "tcp",
+            },
         }
     )
 
 
 @pytest.fixture()
-def security_group_ingress_ipv6_1():
+def security_group_ingress_ipv6():
     return SecurityGroupIngress(
         **{
             "Type": "AWS::EC2::SecurityGroupIngress",
             "Properties": {
                 "GroupId": "sg-12341234",
-                "CidrIpv6": "fc00::/7",
+                "CidrIpv6": "2001:0db8:0000:0000:0000:ff00:0042:8329",
                 "FromPort": 46,
                 "ToPort": 46,
                 "IpProtocol": "tcp",
@@ -54,7 +66,7 @@ def security_group_ingress_ipv6_1():
 
 
 def test_security_group_ingress(security_group_ingress_ipv4_1):
-    assert security_group_ingress_ipv4_1.Properties.GroupId == "sg-1"
+    assert security_group_ingress_ipv4_1.Properties.GroupId == "sg-12341234"
     assert isinstance(security_group_ingress_ipv4_1.Properties.FromPort, int)
 
 
@@ -66,5 +78,5 @@ def test_not_slash_zero4(security_group_ingress_ipv4_2):
     assert security_group_ingress_ipv4_2.ipv4_slash_zero() == False
 
 
-def test_not_slash_zero6(security_group_ingress_ipv6_1):
-    assert security_group_ingress_ipv6_1.ipv6_slash_zero() == False
+def test_not_slash_zero6(security_group_ingress_ipv6):
+    assert security_group_ingress_ipv6.ipv6_slash_zero() == False
