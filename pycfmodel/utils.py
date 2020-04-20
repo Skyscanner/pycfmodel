443ca708115f7055021f9ad86e49011505ecbb3d
Oscar Blanco <oscarbc1996@gmail.com>
2020-04-20 03:14:17 +0200

    add convert_to_list

--- pycfmodel/utils.py
+++ pycfmodel/utils.py
@@ -1,7 +1,13 @@
-from typing import Any
+from typing import Any, List, Union
 
 from pycfmodel.constants import IMPLEMENTED_FUNCTIONS
 
 
 def is_resolvable_dict(value: Any) -> bool:
     return isinstance(value, dict) and len(value) == 1 and next(iter(value)) in IMPLEMENTED_FUNCTIONS
+
+
+def convert_to_list(item: Union[Any, List[Any]]) -> List:
+    if isinstance(item, list):
+        return item
+    return [item]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,16 @@
+from typing import Any, List
+
+import pytest
+
 from pycfmodel.constants import IMPLEMENTED_FUNCTIONS
 from pycfmodel.resolver import FUNCTION_MAPPINGS
+from pycfmodel.utils import convert_to_list
 
 
 def test_implemented_functions():
     assert set(FUNCTION_MAPPINGS) == IMPLEMENTED_FUNCTIONS
+
+
+@pytest.mark.parametrize("param, expected_output", [(1, [1]), ([1], [1]), ("a", ["a"]), (["a"], ["a"])])
+def test_convert_to_list(param: Any, expected_output: List):
+    assert convert_to_list(param) == expected_output
