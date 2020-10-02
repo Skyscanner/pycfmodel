f0fc13bcaaae696c2a5c7f88e206213416943817
Oscar Blanco <oscarbc1996@gmail.com>
2020-10-02 13:32:24 +0200

    update get data script

--- scripts/update_cloudformation_actions_file.py
+++ scripts/update_cloudformation_actions_file.py
@@ -1,3 +1,9 @@
+import os
+import shutil
+from pathlib import Path
+
 from policy_sentry.shared.constants import BUNDLED_DATASTORE_FILE_PATH
 
-print(BUNDLED_DATASTORE_FILE_PATH)
+actions_folder_path = Path(os.path.dirname(__file__)).parent / "pycfmodel" / "actions"
+
+shutil.copy(BUNDLED_DATASTORE_FILE_PATH, actions_folder_path)
