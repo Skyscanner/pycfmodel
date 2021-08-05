643259e54bf854b5f9f35a6d54b6564214cc52bc
Oscar Blanco <oscarbc1996@gmail.com>
2021-08-05 12:35:49 +0200

    remove old validators

--- pycfmodel/model/resources/properties/security_group_egress_prop.py
+++ pycfmodel/model/resources/properties/security_group_egress_prop.py
@@ -1,8 +1,6 @@
 from ipaddress import IPv4Network, IPv6Network
 from typing import Optional
 
-from pydantic import validator
-
 from pycfmodel.constants import IPV4_ZERO_VALUE, IPV6_ZERO_VALUE
 from pycfmodel.model.resources.properties.property import Property
 from pycfmodel.model.types import (
@@ -40,14 +38,6 @@ class SecurityGroupEgressProp(Property):
     FromPort: Optional[ResolvableInt] = None
     IpProtocol: ResolvableIntOrStr
     ToPort: Optional[ResolvableInt] = None
-
-    @validator("CidrIp", pre=True)
-    def set_CidrIp(cls, v):
-        return IPv4Network(v, strict=False)
-
-    @validator("CidrIpv6", pre=True)
-    def set_CidrIpv6(cls, v):
-        return IPv6Network(v, strict=False)
 
     def ipv4_slash_zero(self) -> bool:
         """Returns True if `CidrIp` matches `0.0.0.0/0`, otherwise False."""
