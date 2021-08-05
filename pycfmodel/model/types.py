fa810a6b3a63e34271767becf864a2cef6a9ec6c
Oscar Blanco <oscarbc1996@gmail.com>
2021-08-05 12:37:05 +0200

    add new loose models

--- pycfmodel/model/types.py
+++ pycfmodel/model/types.py
@@ -1,8 +1,40 @@
 from datetime import date, datetime
-from ipaddress import IPv4Network, IPv6Network
-from typing import List, TypeVar, Union
+from ipaddress import IPv4Network, IPv6Network, _BaseNetwork
+from typing import Any, Dict, Generator, List, TypeVar, Union
+
+from pydantic.networks import NetworkType
+from pydantic.typing import AnyCallable
 
 from pycfmodel.model.base import FunctionDict
+
+
+class LooseIPv4Network(_BaseNetwork):
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[str, Any]) -> None:
+        field_schema.update(type="string", format="looseipv4network")
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[AnyCallable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: NetworkType) -> IPv4Network:
+        return IPv4Network(value, False)
+
+
+class LooseIPv6Network(_BaseNetwork):
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[str, Any]) -> None:
+        field_schema.update(type="string", format="looseipv6network")
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[AnyCallable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: NetworkType) -> IPv6Network:
+        return IPv6Network(value, False)
+
 
 T = TypeVar("T")
 
@@ -17,8 +49,8 @@ ResolvableDate = Resolvable[date]
 ResolvableDatetime = Resolvable[datetime]
 ResolvableBool = Resolvable[bool]
 
-ResolvableIPv4Network = Resolvable[IPv4Network]
-ResolvableIPv6Network = Resolvable[IPv6Network]
+ResolvableIPv4Network = Resolvable[LooseIPv4Network]
+ResolvableIPv6Network = Resolvable[LooseIPv6Network]
 
 ResolvableIntOrStr = Resolvable[Union[int, str]]
 
