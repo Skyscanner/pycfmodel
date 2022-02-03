20771603afe905261fac497e89fdd30f7af14b3f
Oscar Blanco Castan <oscarbc1996@gmail.com>
2022-02-03 10:30:54 +0100

    fix formating

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = readme.read_text()
 install_requires = ["pydantic~=1.0"]
 
 dev_requires = [
-    "black>=21.4b0",
+    "black>=22.1.0",
     "flake8>=3.8.3",
     "httpx>=0.14.2",
     "isort>=5.4.2",
