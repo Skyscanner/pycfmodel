9b74b24499a9d2e99d18ff8fccc26516414e6380
Oscar Blanco Castan <oscarbc1996@gmail.com>
2024-05-11 10:24:48 +0200

    wip

--- pycfmodel/model/resources/generic_resource.py
+++ pycfmodel/model/resources/generic_resource.py
@@ -1,9 +1,12 @@
+import logging
 from typing import Optional
 
-from pydantic import ConfigDict
+from pydantic import ConfigDict, field_validator
 
 from pycfmodel.model.generic import Generic
 from pycfmodel.model.resources.resource import Resource
+
+logger = logging.getLogger(__file__)
 
 
 class GenericResource(Resource):
@@ -13,3 +16,14 @@ class GenericResource(Resource):
     Properties: Optional[Generic] = None
 
     model_config = ConfigDict(extra="allow")
+
+    @field_validator("Type", mode="before")
+    def check_type(cls, value, values, **kwargs):
+        from pycfmodel.model.resources.types import ResourceModels
+
+        existing_resource_types = {
+            klass.model_fields["Type"].annotation.__args__[0] for klass in ResourceModels.__args__[0].__args__
+        }
+        if value in existing_resource_types:
+            logger.warning(f"Instantiating a GenericResource from a {value} in {values}")
+        return value
