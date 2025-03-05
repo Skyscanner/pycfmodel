aaf4f90b1b13ff39684333f33a354ed0a8af5ee5
Jordi Soucheiron <jsoucheiron@users.noreply.github.com>
2025-03-05 10:55:44 +0100

    Add the possibility of having non-strict generic resources (#164)
    * Add the possibility of having non-strict generic resources
    
    * Update changelog and docs
    
    * Update version
    
    * Remove changelog entry

--- pycfmodel/model/resources/generic_resource.py
+++ pycfmodel/model/resources/generic_resource.py
@@ -1,18 +1,28 @@
-from typing import Optional
+import logging
+from typing import ClassVar, Optional
 
 from pydantic import ConfigDict, field_validator
 
 from pycfmodel.model.generic import Generic
 from pycfmodel.model.resources.resource import Resource
 
+logger = logging.getLogger(__name__)
+
 
 class GenericResource(Resource):
-    """This class is used for all resource types that don't have a dedicated class."""
+    """
+    This class is used for all resource types that don't have a dedicated class.
+    Setting _strict to False will allow creation of GenericResources that have an explicit model class,
+    this is very useful to work around incomplete model definitions or things that are not implemented yet
+    like AWS:NoValue. _strict defaults to True. In that case you can't instantiate GenericResources when
+    there's a model for that type of resource.
+    """
 
     Type: Optional[str] = None  # Optional to handle cases like "AWS::CloudFormation::Authentication"
     Properties: Optional[Generic] = None
 
     model_config = ConfigDict(extra="allow")
+    _strict: ClassVar[bool] = True
 
     @field_validator("Type", mode="before")
     @classmethod
@@ -22,6 +32,8 @@ class GenericResource(Resource):
         existing_resource_types = {
             klass.model_fields["Type"].annotation.__args__[0] for klass in ResourceModels.__args__[0].__args__
         }
-        if value in existing_resource_types:
+        if value in existing_resource_types and cls._strict:
             raise ValueError(f"Instantiation of GenericResource from {value} in {values} not allowed")
+        else:
+            logger.warning(f"Instantiation of GenericResource from {value} in {values}")
         return value
